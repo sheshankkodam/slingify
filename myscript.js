1f2b2675070c82d22f57ef9bdf1bf02894dea2f4
Adam Lawson <adam.lawson@echostar.com>
2016-12-02 12:53:01 -0700

    Custom Toolbar Turorial 7

--- myscript.js
+++ myscript.js
@@ -1,1 +1,9 @@
-chrome.runtime.sendMessage(document.getElementsByTagName("title")[0].innerText);
+var url = chrome.extension.getURL("toolbar.html");
+var height = '35px';
+var iframe = "<iframe src='"+url+"' id='myOwnCustomFirstToolbar12345' style='height:"+height+"'></iframe>";
+
+$("html").append(iframe);
+
+$('body').css({
+    'transform': 'translateY('+height+')'
+});
