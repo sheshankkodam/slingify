e78560e0ff0571917ac55add355b4814de1afb58
sheshank.kodam <sheshank.kodam@echostar.com>
2016-12-03 23:53:52 -0700

    twittert tk

--- background.js
+++ background.js
@@ -1,10 +1,18 @@
 var tweetMessages = [];
+var tweetImages = [];
 var dataItemId = [];
 var latestId;
 var latestTweet;
+var latestTweetImage;
 var newTweets = [];
 
+
+
 $(function () {
+    engine();
+    // setInterval(engine, 2000)
+});
+function engine() {
     $.get("https://twitter.com/slingify", function (data) {
         var htmlData = data;
         $data = $(htmlData).find('#stream-items-id').eq(0);
@@ -12,22 +20,39 @@ $(function () {
 
         for(var i=0; i< $data.find('li.stream-item').length; i++){
             dataItemId[i] = $data.find('li.stream-item').eq(0).attr('data-item-id');
-            tweetMessages[i] = ($($data).find('li.stream-item').eq(i).find('div.js-tweet-text-container').text()).replace(/\n/g,'').trim();
+            // tweetMessages[i] = ($($data).find('li.stream-item').eq(i).find('div.js-tweet-text-container').find('p.tweet-text').text()).replace(/\n/g,'').trim();
+            tweetMessages[i] = ($($data).find('li.stream-item').eq(i).find('div.js-tweet-text-container').find('p.tweet-text')).text().replace('pic.twitter.com/','');
+            tweetImages[i] = ($($data).find('li.stream-item').eq(i).find('div.AdaptiveMedia-photoContainer').attr('data-image-url'));
         }
 
         latestId = dataItemId[0];
         latestTweet = tweetMessages[0];
+        latestTweetImage = tweetImages[0];
 
-        // if(latestId == dataItemId[0]){
-        //     //no update
-        // } else if (latestId === undefined){
-        //
-        // } else if (latestId != dataItemId[0]){
-        //
-        // }
-        console.log(latestId);
-        console.log(latestTweet);
-    })
-});
+        if(latestTweetImage === undefined) {
+            var options = {
+                type: "basic",
+                title: "Slingify-Twitter Notifier",
+                iconUrl: "images/slingify_logo1.png",
+                message: latestTweet
+            };
+            chrome.notifications.create(options);
+        } else {
+            var options = {
+                type: "image",
+                title: "Slingify-Twitter Notifier",
+                iconUrl: "images/slingify_logo1.png",
+                message: latestTweet,
+                imageUrl: "https://pbs.twimg.com/media/CyXsBrTXcAA9cch.jpg"
+            };
+            chrome.notifications.create(options);
+        }
+    });
+
+
+    //
+
+    // chrome.notifications.onClicked.addListener(replyBtnClick);
+}
 
 
