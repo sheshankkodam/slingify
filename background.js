9783c773806fcda6fa2cf281172cafe2413e15fa
sheshank.kodam <sheshank.kodam@echostar.com>
2016-12-04 00:23:22 -0700

    twittert tk

--- background.js
+++ background.js
@@ -4,13 +4,13 @@ var dataItemId = [];
 var latestId;
 var latestTweet;
 var latestTweetImage;
-var newTweets = [];
+var newTweet;
 
 
 
 $(function () {
     engine();
-    // setInterval(engine, 2000)
+    setInterval(engine, 2000)
 });
 function engine() {
     $.get("https://twitter.com/slingify", function (data) {
@@ -19,34 +19,46 @@ function engine() {
         $('body').append($data)
 
         for(var i=0; i< $data.find('li.stream-item').length; i++){
-            dataItemId[i] = $data.find('li.stream-item').eq(0).attr('data-item-id');
+            dataItemId[i] = $data.find('li.stream-item').eq(i).attr('data-item-id');
             // tweetMessages[i] = ($($data).find('li.stream-item').eq(i).find('div.js-tweet-text-container').find('p.tweet-text').text()).replace(/\n/g,'').trim();
             tweetMessages[i] = ($($data).find('li.stream-item').eq(i).find('div.js-tweet-text-container').find('p.tweet-text')).text().replace('pic.twitter.com/','');
             tweetImages[i] = ($($data).find('li.stream-item').eq(i).find('div.AdaptiveMedia-photoContainer').attr('data-image-url'));
         }
 
-        latestId = dataItemId[0];
-        latestTweet = tweetMessages[0];
-        latestTweetImage = tweetImages[0];
-
-        if(latestTweetImage === undefined) {
-            var options = {
-                type: "basic",
-                title: "Slingify-Twitter Notifier",
-                iconUrl: "images/slingify_logo1.png",
-                message: latestTweet
-            };
-            chrome.notifications.create(options);
-        } else {
-            var options = {
-                type: "image",
-                title: "Slingify-Twitter Notifier",
-                iconUrl: "images/slingify_logo1.png",
-                message: latestTweet,
-                imageUrl: "https://pbs.twimg.com/media/CyXsBrTXcAA9cch.jpg"
-            };
-            chrome.notifications.create(options);
+        if(latestId === undefined || latestId !== dataItemId[0]){
+            //during first run
+            latestId = dataItemId[0];
+            latestTweet = tweetMessages[0];
+            latestTweetImage = tweetImages[0];
+            newTweet = true;
+        } else if(latestId === dataItemId[0]){
+            // not a new tweet
+            newTweet = false;
         }
+
+        if(newTweet === true){
+            if(latestTweetImage === undefined) {
+                var options = {
+                    type: "basic",
+                    title: "Slingify-Twitter Notifier",
+                    iconUrl: "images/slingify_logo1.png",
+                    message: latestTweet
+                };
+                chrome.notifications.create(options);
+            } else {
+                var options = {
+                    type: "image",
+                    title: "Slingify-Twitter Notifier",
+                    iconUrl: "images/slingify_logo1.png",
+                    message: latestTweet,
+                    imageUrl: "https://pbs.twimg.com/media/CyXsBrTXcAA9cch.jpg"
+                };
+                chrome.notifications.create(options);
+            }
+        } else {
+            //do nothing
+        }
+
     });
 
 
